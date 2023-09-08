c338246baf2e5e283d918c606552a0335a3b723d
Ahsan <sbaseer25@gmail.com>
2023-09-08 12:33:31 -0400

    revert changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3e1ac33d04de013a"; }
+/******/ 		__webpack_require__.h = function() { return "e6777d0d5a37f548"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="bg-blue-950 py-4">
       <div className="container mx-auto">
-        <h1 className="text-white text-2xl font-semibold text-center">AI-Assistant powered by GPT</h1>
+        <h1 className="text-white text-2xl font-semibold text-center">Chatter-Bot powered by GPT</h1>
       </div>
     </header>
   );
