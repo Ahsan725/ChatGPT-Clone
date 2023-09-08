a9a1beb84d905787082a705b5c7ce9b589ae5e48
Ahsan <sbaseer25@gmail.com>
2023-09-07 23:46:07 -0400

    Vercel ready

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,14 @@
+// Header.js
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-blue-950 py-4">
+      <div className="container mx-auto">
+        <h1 className="text-white text-2xl font-semibold text-center">AI-Assistant powered by GPT</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/app/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { useChat } from "ai/react";
+import GPTLogo from "./components/GPTLogo";
+import Header from "./components/Header";
+export default function Home() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  return (
+    <div className="pt-4 pb-32">
+      <Header />
+      {messages.map((message) => (
+        <div
+          key={message.id}
+          className={`border-t border-black/10 ${
+            message.role === "assistant" && "bg-gray-100"
+          }`}
+        >
+          <div className="max-w-3xl mx-auto py-6 flex">
+            {message.role === "assistant" && <GPTLogo />}
+            <span className="ml-3">{message.content}</span>
+          </div>
+        </div>
+      ))}
+
+      <form className="fixed inset-x-0 bottom-10" onSubmit={handleSubmit}>
+        <input
+          className="max-w-3xl shadow-xl w-full mx-auto py-8 flex h-10 rounded-md border border-input bg-background px-3 text-sm ring-offset-background"
+          placeholder="Harness the power of Artificial Intelligence!"
+          value={input}
+          onChange={handleInputChange}
+        />
+      </form>
+    </div>
+  );
+}
